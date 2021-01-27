fba06134662fc4191b04fe09e2f8a8c4d6dd4b41
2dgradient <78096047+2dgradient@users.noreply.github.com>
2021-01-28 00:32:18 +0100

    Add files via upload

--- script/background.js
+++ script/background.js
@@ -22,20 +22,24 @@ function setCanvasSize() {
 }
 
 function initColors() {
-    function randomColor() {
+    function rcrd(length) {
+        return Math.floor(Math.random() * (length - size * 2)) + size;
+    }
+    
+    function rclr() {
         let color = [];
         for (let i = 0; i < 3; ++i)
             color.push(Math.floor(Math.random() * 256));
         return color;
     }
 
-    function randomCoord(length) {
-        return Math.floor(Math.random() * (length - size * 2)) + size;
+    function rd() {
+        return Math.random() - 0.5;
     }
 
     colors = [];
     for (let i = 0; i < points; ++i)
-        colors.push({'x': randomCoord(canvas.width), 'y': randomCoord(canvas.height), 'c': randomColor()});
+        colors.push({'x': rcrd(canvas.width), 'y': rcrd(canvas.height), 'c': rclr(), 'dx': rd(), 'dy': rd(), 'dc': [rd(), rd(), rd()]});
 }
 
 function drawColors() {
@@ -51,8 +55,26 @@ function drawColors() {
 }
 
 function animate() {
-    
-    //draw();
+    function oob(v, mi, ma) {
+        return v < mi || v > ma;
+    }
+
+    let cm = 10;
+    let pm = 5;
+    for (let i = 0; i < colors.length; ++i) {
+        const c = colors[i];
+        colors[i].x += c.dx * pm;
+        colors[i].y += c.dy * pm;
+        if (oob(c.x, 0, canvas.width)) colors[i].dx *= -1;
+        if (oob(c.y, 0, canvas.height)) colors[i].dy *= -1;
+        for (let j = 0; j < 3; ++j) {
+            if (oob(c.c[j] + c.dc[j] * cm, 0, 255))
+                colors[i].dc[j] *= -1;
+            colors[i].c[j] += colors[i].dc[j] * cm;
+        }
+    }
+
+    draw();
 }
 
 function draw() {
@@ -64,12 +86,12 @@ function draw() {
             grd.addColorStop(0, rgba(clr, 1));
             grd.addColorStop(1, rgba(clr, 0));
             ctx.fillStyle = grd;
-            ctx.fillRect(x - size * overflow, y - size * overflow, canvas.width - x + size * overflow, canvas.height - y + size * overflow);
+            ctx.fillRect(x - size * overflow, y - size * overflow, size * overflow * 2, size * overflow * 2);
         }
     }
 }
 
-function drawFull() {
+/* function drawFull() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let img = ctx.createImageData(canvas.width, canvas.height);
     for (let i = 0; i < img.data.length; i += 4) {
@@ -79,7 +101,7 @@ function drawFull() {
         img.data[i + 3] = 255;
     }
     ctx.putImageData(img, 0, 0);
-}
+} */
 
 function gradientColor(x, y) {
     function angle(x1, y1, x2, y2, x3, y3) {
